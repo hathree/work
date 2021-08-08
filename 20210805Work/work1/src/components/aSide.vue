631f5f04f1d4d992c692ff1a0a0f492c294a9a27
glj <51580507+hathree@users.noreply.github.com>
2021-08-08 13:31:24 +0800

    Add files via upload

---
+++ 20210805Work/work1/src/components/aSide.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="aSide">
+    <div class="navMenu" v-for="item in navMenu" :key="item.title" @click="selected = item.title">
+      <router-link :to="item.router">
+        <div class="navMenuItem">
+          <i :class="item.icon"></i> 
+          <span> {{ item.title }}</span>
+        </div>
+      </router-link>
+
+      <ul class="subNavMenu" :style="{display: selected === item.title ? 'block' : 'none'}">
+        <li
+          class="subNavMenuItem"
+          v-for="subItem in item.subNavMenu"
+          :key="subItem.title"
+        >
+          <router-link :to="subItem.router">
+            <i :class="subItem.icon"></i>
+            <span> {{ subItem.title }}</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "aSide",
+  props: ["navMenu"],
+  data() {
+    return {
+        selected: "",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+ul {
+    list-style: none;
+}
+.router-link-active{
+    text-decoration: none;
+    color: red;
+}
+ul .router-link-active{
+    text-decoration: none;
+    color:blue;
+}
+.navMenuItem {
+    font-size: 20px;
+}
+.subNavMenuItem {
+    font-size: 18px;
+}
+
+</style>
